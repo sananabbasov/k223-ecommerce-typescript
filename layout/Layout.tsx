efe3ab8438b21cd5f2a8fdc0af9ea12bcac0a92c
sananabbasov <sanan_abbasov@list.ru>
2024-03-07 14:56:54 +0400

    test

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
 
@@ -6,7 +7,7 @@ function Layout({ children }: { children: React.ReactNode }) {
         <>
             <Header />
             {children}
-            Footer
+            <Footer />
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
+import ProductCard from '@/components/ProductCard'
 import React from 'react'
+import '@/app/globals.css'
+import Slider from '@/components/Slider'
 
-function index() {
+interface Product{
+    id: number,
+    productName: string,
+    price: number,
+    photoUrl: string
+}
+
+
+function index({ products }: { products: Product[] }) {
   return (
-    <div>index</div>
+    <div>
+      <Slider />
+      <ProductCard products={products} />
+    </div>
   )
 }
 
 export default index
+
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://192.168.110.249:8080/api/v1/Product/featured`)
+  const result = await res.json()
+  const products = result.data;
+
+
+
+  // Pass data to the page via props
+  return { props: { products } }
+}
